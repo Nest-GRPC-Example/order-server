ab5734975171ff0d342cecb7573f40dd1818d99b
Ko-GyeongTae <kokt0203@naver.com>
2022-06-05 00:55:14 +0900

    Feat: ENV 추가

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     {
       transport: Transport.GRPC,
       options: {
-        url: '0.0.0.0:50052',
+        url: `${process.env.MSA_HOST}:${process.env.MSA_PORT}`,
         package: protobufPackage,
         protoPath: join('node_modules/grpc-nest-proto/proto/order.proto'),
       },
